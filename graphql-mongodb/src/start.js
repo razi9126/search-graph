6279c9af924cd715be3fc504b1c74fa3637dc608
razi9126 <raziulhaq@Razi-ul-Haq.local>
2019-07-28 00:00:57 +0500

    Changed structure to brong server to root folder

--- graphql-mongodb/src/start.js
+++ graphql-mongodb/src/start.js
@@ -164,6 +164,16 @@ app.listen(PORT, () => {
   console.log(`Visit ${URL}:${PORT}${homePath}`)
 })
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'react-graphql/react-apollo-ap/build')));
+    
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'react-graphql/react-apollo-ap/build', 'index.html'));
+  });
+}
+
 } catch (e) {
   console.log(e)
 }
