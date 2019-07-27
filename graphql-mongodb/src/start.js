c047b326e2d799f3539ad60fae28c8afb15709d4
razi9126 <raziulhaq@Razi-ul-Haq.local>
2019-07-28 00:56:58 +0500

    Changed structure to bring server to root folder

--- graphql-mongodb/src/start.js
+++ graphql-mongodb/src/start.js
@@ -10,6 +10,10 @@ import {prepare} from "../util/index"
 const app = express()
 
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/react-graphql/react-apollo-ap/build')))
+
 
 const homePath = '/graphiql'
 const URL = 'http://localhost'
@@ -161,11 +165,11 @@ app.use(homePath, graphiqlExpress({
   endpointURL: '/graphql'
 }))
 
-app.use(express.static(path.join(__dirname, '/react-graphql/react-apollo-ap/build')));
+app.use(express.static(path.join(__dirname, 'react-graphql/react-apollo-ap/build')));
   
 // Handle React routing, return all requests to React app
 app.get('*', function(req, res) {
-  res.sendFile('index.html', {root: path.join(__dirname, '/react-graphql/react-apollo-ap/build')});
+  res.sendFile('index.html', {root: path.join(__dirname, 'react-graphql/react-apollo-ap/build')});
   console.log(__dirname);
 });
 
